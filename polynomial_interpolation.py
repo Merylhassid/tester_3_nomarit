9a1f5dc7bf3f61464e8bd3d8ff684792801a9566
Merylhassid <157501246+Merylhassid@users.noreply.github.com>
2024-04-07 23:25:50 +0300

    Add files via upload

---
+++ polynomial_interpolation.py
@@ -0,0 +1,30 @@
+from colors import bcolors
+from matrix_utility import *
+import numpy as np
+
+def polynomialInterpolation(table_points, x):
+    matrix = [[point[0] ** i for i in range(len(table_points))] for point in table_points] # Makes the initial matrix
+
+    b = [[point[1]] for point in table_points]
+
+    print(bcolors.OKBLUE, "The matrix obtained from the points: ", bcolors.ENDC,'\n', np.array(matrix))
+    print(bcolors.OKBLUE, "\nb vector: ", bcolors.ENDC,'\n',np.array(b))
+    matrixSol = np.linalg.solve(matrix,b) #solveMatrix(matrix, b)
+
+    result = sum([matrixSol[i][0] * (x ** i) for i in range(len(matrixSol))])
+    print(bcolors.OKBLUE, "\nThe polynom:", bcolors.ENDC)
+    print('P(X) = '+'+'.join([ '('+str(matrixSol[i][0])+') * x^' + str(i) + ' ' for i in range(len(matrixSol))])  )
+    print(bcolors.OKGREEN, f"\nThe Result of P(X={x}) is:", bcolors.ENDC)
+    print(result)
+    return result
+
+
+if __name__ == '__main__':
+
+    table_points = [(1, 3), (2, 4), (3, -1)]
+    x = 1.5
+    print(bcolors.OKBLUE, "----------------- Interpolation & Extrapolation Methods -----------------\n", bcolors.ENDC)
+    print(bcolors.OKBLUE, "Table Points: ", bcolors.ENDC, table_points)
+    print(bcolors.OKBLUE, "Finding an approximation to the point: ", bcolors.ENDC, x,'\n')
+    polynomialInterpolation(table_points, x)
+    print(bcolors.OKBLUE, "\n---------------------------------------------------------------------------\n", bcolors.ENDC)
