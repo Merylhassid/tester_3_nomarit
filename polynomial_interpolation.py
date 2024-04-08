f14609086a484e53e5c3a0d0a87526ff22e84c33
Merylhassid <157501246+Merylhassid@users.noreply.github.com>
2024-04-08 15:03:44 +0300

    Update polynomial_interpolation.py

--- polynomial_interpolation.py
+++ polynomial_interpolation.py
@@ -7,13 +7,13 @@ def polynomialInterpolation(table_points, x):
 
     b = [[point[1]] for point in table_points]
 
-    print(bcolors.OKBLUE, "The matrix obtained from the points: ", bcolors.ENDC,'\n', np.array(matrix))
-    print(bcolors.OKBLUE, "\nb vector: ", bcolors.ENDC,'\n',np.array(b))
+    # print(bcolors.OKBLUE, "The matrix obtained from the points: ", bcolors.ENDC,'\n', np.array(matrix))
+    # print(bcolors.OKBLUE, "\nb vector: ", bcolors.ENDC,'\n',np.array(b))
     matrixSol = np.linalg.solve(matrix,b) #solveMatrix(matrix, b)
 
     result = sum([matrixSol[i][0] * (x ** i) for i in range(len(matrixSol))])
-    print(bcolors.OKBLUE, "\nThe polynom:", bcolors.ENDC)
-    print('P(X) = '+'+'.join([ '('+str(matrixSol[i][0])+') * x^' + str(i) + ' ' for i in range(len(matrixSol))])  )
+    # print(bcolors.OKBLUE, "\nThe polynom:", bcolors.ENDC)
+    # print('P(X) = '+'+'.join([ '('+str(matrixSol[i][0])+') * x^' + str(i) + ' ' for i in range(len(matrixSol))])  )
     print(bcolors.OKGREEN, f"\nThe Result of P(X={x}) is:", bcolors.ENDC)
     print(result)
     return result
@@ -22,10 +22,13 @@ def polynomialInterpolation(table_points, x):
 if __name__ == '__main__':
     print("https://github.com/Merylhassid/tester_3_nomarit\ngroup:Almog Babila 209477678, Hai karmi 207265678, Yagel Batito 318271863, Meryl Hassid 324569714\nstudent:Meryl Hassid 324569714")
 
-    table_points = [(1, 1), (1.3, 1), (1.4, 1)]
+    table_points = [(1.2, 1.5095), (1.3, 1.6984), (1.4, 1.9043), (1.5, 2.1293), (1.6, 2.3756)]
     x = 1.47
-    print(bcolors.OKBLUE, "----------------- Interpolation & Extrapolation Methods -----------------\n", bcolors.ENDC)
-    print(bcolors.OKBLUE, "Table Points: ", bcolors.ENDC, table_points)
-    print(bcolors.OKBLUE, "Finding an approximation to the point: ", bcolors.ENDC, x,'\n')
+    y = 1.67
+    # print(bcolors.OKBLUE, "----------------- Interpolation & Extrapolation Methods -----------------\n", bcolors.ENDC)
+    # print(bcolors.OKBLUE, "Table Points: ", bcolors.ENDC, table_points)
+    # print(bcolors.OKBLUE, "Finding an approximation to the point: ", bcolors.ENDC, x,'\n')
     polynomialInterpolation(table_points, x)
+    polynomialInterpolation(table_points, y)
+
     print(bcolors.OKBLUE, "\n---------------------------------------------------------------------------\n", bcolors.ENDC)
